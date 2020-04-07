6685ed03c18461e765d7bc50b83fa462ed9c4c1a
Jan Lewandowski <iasiu@icloud.com>
2020-04-07 14:21:17 +0200

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Jan Lewandowski on 07/04/2020.
+//  Copyright © 2020 janlewandowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
